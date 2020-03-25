a5c47c786ee8fd342578e20e172b1d99e0c6a941
puge <mail@puge.org>
2020-03-25 14:35:04 +0800

    save

--- src/main.js
+++ src/main.js
@@ -9,16 +9,24 @@ function getScrollTop() {
   scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
   return scrollTop;
 }
+setTimeout(() => {
+  if (screenInfo.isPc) {
+    var drop = true
+    
+    document.querySelector('.nav.clearfix').style.top = '50px'
+    window.onscroll = function(){
+      var scrollTopValue = getScrollTop()
+      if (scrollTopValue > 50 && drop) {
+        document.querySelector('.nav.clearfix').style.top = '0px'
+        document.querySelector('.nav.clearfix').classList.add('drop')
+        drop = false
+      }
+      if (scrollTopValue <= 50 && !drop) {
+        document.querySelector('.nav.clearfix').classList.remove('drop')
+        drop = true
+      }
+    }
+  }
+}, 0);
 
-var drop = true
-window.onscroll = function(){
-  var scrollTopValue = getScrollTop()
-  if (scrollTopValue > 50 && drop) {
-    document.querySelector('.nav.clearfix').classList.add('drop')
-    drop = false
-  }
-  if (scrollTopValue <= 50 && !drop) {
-    document.querySelector('.nav.clearfix').classList.remove('drop')
-    drop = true
-  }
-}
+
